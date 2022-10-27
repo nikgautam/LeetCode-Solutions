51fe8417f481594d68c8b7bbf8142a0d4d8f88c5
Nikhil <37222538+nikgautam@users.noreply.github.com>
2022-10-27 23:52:00 +0530

    Time: 1 ms (93.60%), Space: 42 MB (62.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,29 +1,45 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
-        String ans = "";
-        for( int j=0; j<200; j++ ){
-            boolean match = true;
-            for( int i=0; i<strs.length; i++ ){
-                if( j >= strs[i].length() ){
-                    match = false;
-                    break;
-                }
-                
-                if( strs[0].charAt(j) != strs[i].charAt(j) ){
-                    match = false;
-                    break;
-                }
-                
-            }
+        //String ans = "";
+        Arrays.sort(strs);
+        
+        int c = 0;
+        String first = strs[0];
+        String last = strs[ strs.length-1 ];
+        
+        while( c<first.length() && c<last.length() ){
             
-            if( match )
-                ans = ans + strs[0].charAt(j);
-            else
-                break;
+            if( first.charAt(c) == last.charAt(c) )
+                c++;
+            else break;
             
         }
         
-        return ans;
+        return ( c==0 ) ? "" : first.substring(0,c);
+        
+//         for( int j=0; j<200; j++ ){
+//             boolean match = true;
+//             for( int i=0; i<strs.length; i++ ){
+//                 if( j >= strs[i].length() ){
+//                     match = false;
+//                     break;
+//                 }
+                
+//                 if( strs[0].charAt(j) != strs[i].charAt(j) ){
+//                     match = false;
+//                     break;
+//                 }
+                
+//             }
+            
+//             if( match )
+//                 ans = ans + strs[0].charAt(j);
+//             else
+//                 break;
+            
+//         }
+        
+        //return ans;
     }
 }
